de7ddd6867305509ad04e27d77a152430780f75f
Devin Koehl <devink@uab.edu>
2019-12-08 15:31:24 -0600

    edit schema

--- Create.sql
+++ Create.sql
@@ -1,7 +1,9 @@
 
 
-DROP SCHEMA music_shop2 CASCADE;
-CREATE SCHEMA music_shop2;
+--DROP SCHEMA music_shop2 CASCADE;
+--CREATE SCHEMA music_shop2;
+
+set schema 'music_shop2';
 
 CREATE TABLE Courier (
    CourierID varchar(100) NOT NULL PRIMARY KEY,
